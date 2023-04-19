c0fc29dbda173bffac0b3d12ea22113466aab08c
LastWeekNextDay <56513101+LastWeekNextDay@users.noreply.github.com>
2023-04-19 13:58:33 +0300

    added functionality to delete account button, implemented abstract class

--- src/main/java/lt/viko/eif/nlavkart/internetShopClient/GUI/AccountWindow.java
+++ src/main/java/lt/viko/eif/nlavkart/internetShopClient/GUI/AccountWindow.java
@@ -1,26 +1,28 @@
 package lt.viko.eif.nlavkart.internetShopClient.GUI;
 
 import lt.viko.eif.nlavkart.internetShopClient.SOAP.Account;
+import lt.viko.eif.nlavkart.internetShopClient.SOAP.RemoveAccountRequest;
+import lt.viko.eif.nlavkart.internetShopClient.SOAP.forClient.InteractClass;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 public class AccountWindow {
-    private Account account;
 
-    private int index;
     private JPanel windowPane;
     private JButton cartButton;
     private JButton deleteButton;
     private JButton printButton;
     private JLabel nameLabel;
 
-    public AccountWindow(int index, Account account) {
-        this.index = index;
-        this.account = account;
+    AbstractWindow previousWindow;
+
+    public AccountWindow(Account account, ArrayList<Account> listOfAccounts, AbstractWindow previousWindow) {
+        this.previousWindow = previousWindow;
         JFrame frame = new JFrame("AccountWindow");
         frame.setContentPane(windowPane);
         nameLabel.setText(account.getUsername());
@@ -32,7 +34,14 @@ public class AccountWindow {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                // delete account
+                InteractClass interactClass = new InteractClass();
+                interactClass.init();
+                RemoveAccountRequest request = new RemoveAccountRequest();
+                request.setAccountId(account.getId());
+                interactClass.removeAccount(request);
+                listOfAccounts.remove(account);
+                previousWindow.update();
+                frame.dispose();
             }
         });
         cartButton.addMouseListener(new MouseAdapter() {
